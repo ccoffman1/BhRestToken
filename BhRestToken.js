f762e6d002ce09ed429133de5515562250072fd9
Colin Coffman <crcoffman1@gmail.com>
2018-04-22 17:56:26 -0500

    first commit + BhRestTokenUrl

---
+++ BhRestToken.js
@@ -0,0 +1,11 @@
+//https://rest.bullhornstaffing.com/rest-services/login?version=*&access_token={xxxxxxxx}
+
+// import BhAccessToken.js
+// GET request
+
+// User access_token to generate BhRestToken
+
+var BhRestTokenUrl = 'https://rest.bullhornstaffing.com/rest-services/login?version=*&access_token=' + access_token
+
+
+// run GET request, receive back BhRestToken and restUrl
